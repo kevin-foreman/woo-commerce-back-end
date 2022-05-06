a198c643856b5aca954cac82a8f7dc0bb9d4e418
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-05-06 11:09:24 -0500

    Started work on models section with index.js

--- models/index.js
+++ models/index.js
@@ -1,16 +1,32 @@
 // import models
-const Product = require('./Product');
 const Category = require('./Category');
+const Product = require('./Product');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+    foreignKey: 'product_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+    foreignKey: 'category_id'
+    onDelete: 'SET NULL'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+    through: ProductTag,
+    as: 'tagged_product'
+    foreignKey: '?'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+    through: ProductTag,
+    as: '?'
+})
 
 module.exports = {
     Product,
