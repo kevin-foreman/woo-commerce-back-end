435afc66b6a07b6b5d68b4269256e78c08abb5f9
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-05-06 12:15:55 -0500

    Found missing information in models/index.js added ProductTag model

--- models/index.js
+++ models/index.js
@@ -11,25 +11,30 @@ Product.belongsTo(Category, {
 
 // Categories have many Products
 Category.hasMany(Product, {
-    foreignKey: 'category_id'
+    foreignKey: 'category_id',
     onDelete: 'SET NULL'
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
     through: ProductTag,
-    as: 'product_tag'
-    foreignKey: 'product_id'
+    as: 'product_tag',
+    foreignKey: 'product_id',
     onDelete: 'SET NULL'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
     through: ProductTag,
-    as: 'product_tag'
-    foreignKey: 'product_id'
+    as: 'product_tag',
+    foreignKey: 'product_id',
     onDelete: 'SET NULL'
-})
+});
+
+ProductTag.belongsTo(Product, {
+    foreignKey: 'product.id',
+    onDelete: 'SET NULL'
+});
 
 module.exports = {
     Product,
