32c7f110a80a5b1b99379d68ed00f17175463c1c
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-05-06 23:09:13 -0500

    refactored some of the model logic, but bugs still persist

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -47,7 +47,7 @@ router.get('/:id', (req, res) => {
             {
                 model: Tag,
                 attributes: ['id', 'tag_name'],
-                through: productTag,
+                through: ProductTag,
             },
         ],
     })
@@ -64,21 +64,15 @@ router.get('/:id', (req, res) => {
         });
 });
 
-
-
-
-
-
-
 // create new product
 router.post('/', (req, res) => {
     /* req.body should look like this...
-      {
+        {
         product_name: "Basketball",
         price: 200.00,
         stock: 3,
         tagIds: [1, 2, 3, 4]
-      }
+        }
     */
     Product.create(req.body)
         .then((product) => {
